cddf1e6c4386b9ff500f98638c1f5c2157c11599
salmahammad436 <sayahammad@gmail.com>
2025-02-16 00:12:02 +0200

    ok

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 console.log('hello from mariam');
 // const o=0; 
-const x = 'conflict from mariam';
+const x = 'conflict from mariam'
 // const x = 'conflict from mariam';
 
 // const x = 'conflict from mariam';
